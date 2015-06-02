ea742c67e985e23e01bb0b7235fddbf4d76f6d75
AlexanderMladenov <alexander.r.mladenov@gmail.com>
2015-06-02 12:33:49 +0300

    sample julia fractals

--- fract/src/main.rs
+++ fract/src/main.rs
@@ -1,206 +1,53 @@
+//!An example of generating julia fractals.
+
+extern crate num;
 extern crate image;
-extern crate rand;
 
-use rand::Rng;
+use std::fs::File;
+use std::path::Path;
 
-static fract_seq: [usize; 5] = [1, 1, 0, 0, 1];
-
-fn r(n: usize, x: f32, y: f32) -> f32 {
-
-    let fract_r = n % fract_seq.len();
-    let choice = fract_seq[fract_r];
-    if choice == 1 { x } else { y }
-}
-
-fn compute_lyapunov_exp(x: f32, y: f32) -> f32 {
-    
-    let iterations = 1..500;
-    
-    let mut iter_seq = [0.0; 500];
-    iter_seq[0] = 0.5;
-
-    for i in &mut iter_seq[..] {
-        i = 
-    }
-
-    0.0
-}
+use num::complex::Complex;
 
 fn main() {
+    let max_iterations = 256u16;
 
-    /*
-float computeLyapunovExponent(const vec2& point)
-{
-    auto& p = point;
+    let imgx = 800;
+    let imgy = 800;
 
-    float iter[ITERATIONS];
-    iter[0] = 0.5f;
+    let scalex = 4.0 / imgx as f32;
+    let scaley = 4.0 / imgy as f32;
 
-    for (int i = 1; i < ITERATIONS; i++)
-    {
-        iter[i] = r(i - 1, p) * iter[i - 1] * (1 - iter[i - 1]);
-    }
+    // Create a new ImgBuf with width: imgx and height: imgy
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
 
-    float result = 0.f;
-    float ri[ITERATIONS];
-    for (auto i = 1; i < ITERATIONS; i++)
-    {
-        ri[i] = abs(r(i, p));
-    }
+    // Iterate over the coordiantes and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let cy = y as f32 * scaley - 2.0;
+        let cx = x as f32 * scalex - 2.0;
 
-    float oneMinus2iterations[ITERATIONS];
-    for (auto i = 1; i < ITERATIONS; i++) // vectorized
-    {
-        oneMinus2iterations[i] = abs((1 - (2 * iter[i])));
-    }
+        let mut z = Complex::new(cx, cy);
+        let c = Complex::new(-0.4, 0.6);
 
-    for (int i = 1; i < ITERATIONS; i++) // vectorized
-    {
-        result += logf(ri[i] * oneMinus2iterations[i]);
-    }
+        let mut i = 0;
 
-    return (result / (float) ITERATIONS);
-}
-
-void renderFractalPixel(int x, int y)
-{
-    float a = (float) x / (float) (FRAME_RES);
-    a = 2.f + a * 2.f;
-
-    float b = (float) y / (float) (FRAME_RES);
-    b = 2.f + b * 2.f;
-    
-    auto lyapunovExp = computeLyapunovExponent(vec2(a, b));
-    if (lyapunovExp < 0)
-    {
-        frameR[x][y] = abs(lyapunovExp);
-        frameG[x][y] = abs(lyapunovExp);
-        frameB[x][y] = 0.f;
-    }
-    else if (lyapunovExp == 0.f)
-    {
-        frameR[x][y] = 1;
-        frameG[x][y] = 1;
-        frameB[x][y] = 0.f;
-    }
-    else
-    {
-        frameR[x][y] = 0;
-        frameG[x][y] = lyapunovExp * 0.5f;
-        frameB[x][y] = lyapunovExp;
-    }
-}
-
-void renderFractalRegion(int xFrom, int xTo)
-{
-    for (auto x = xFrom; x < xTo; x++)
-    {
-        for (auto y = 0; y < FRAME_RES; y++)
-        {
-            renderFractalPixel(x, y);
+        for t in (0..max_iterations) {
+            if z.norm() > 2.0 {
+                break
+            }
+            z = z * z + c;
+            i = t;
         }
-    }
-    return;
-}
 
-std::vector<std::thread> threads;
-void DoFractalThreaded()
-{
-    unsigned int numThreads = std::thread::hardware_concurrency();
-    int delta = FRAME_RES / numThreads;
-    for (int i = 0; i < numThreads; i++)
-    {
-        auto fun = std::bind(&renderFractalRegion, delta * i, delta * i + delta);
-        threads.emplace_back(fun);
-    }
-}
+        // Create an 8bit pixel of type Luma and value i
+        // and assign in to the pixel at position (x, y)
+        *pixel = image::Luma([i as u8]);
 
-typedef  std::chrono::time_point<std::chrono::high_resolution_clock> TimePoint;
-template <typename T = std::chrono::seconds>
-inline long long timePast(const TimePoint& start, const TimePoint& end)
-{
-    return std::chrono::duration_cast<T>(end - start).count();
-}
-
-#undef main
-int main(int argc, char* argv[])
-{
-    if (argc == 2)
-    {
-        fractString = argv[1];
     }
 
-    std::cout << "Lyapunov Fractal Renderer. Version: " << VERSION << std::endl;
-    auto begin = std::chrono::high_resolution_clock::now();
-    if (SDL_Init(SDL_INIT_EVERYTHING != 0))
-    {
-        std::cout << "SDL_init failed: " << SDL_GetError() << std::endl;
-        waitForUserExit();
-        SDL_DestroyWindow(m_Window);
-        SDL_Quit();
-        return -1;
-    }
 
-    m_Window = SDL_CreateWindow("LyapunovFract", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                FRAME_RES, FRAME_RES, SDL_WINDOW_OPENGL);
+    // Save the image as “fractal.png”
+    let ref mut fout = File::create(&Path::new("fractal.png")).unwrap();
 
-    if (!m_Window)
-    {
-        std::cout << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
-        waitForUserExit();
-        SDL_DestroyWindow(m_Window);
-        SDL_Quit();
-        return -2;
-    }
-
-    m_Surface = SDL_GetWindowSurface(m_Window);
-
-    if (!m_Surface)
-    {
-        std::cout << "SDL_GetWindowSurface failed: " << SDL_GetError() << std::endl;
-        waitForUserExit();
-        SDL_DestroyWindow(m_Window);
-        SDL_Quit();
-        return -3;
-    }
-    redShift = m_Surface->format->Rshift;
-    greenShift = m_Surface->format->Gshift;
-    blueShift = m_Surface->format->Bshift;
-
-    SwapBuffers();
-    auto end = std::chrono::high_resolution_clock::now();
-    
-    auto timeSec = timePast(begin, end);
-    auto timeMili = timePast<std::chrono::milliseconds>(begin, end);
-    std::cout << "Seting up SDL took: " << timeSec << " sec " << timeMili - (timeSec * 1000) << " milisec" << std::endl;
-
-    std::cout << "Starting rendering with: " << std::thread::hardware_concurrency() << " threads." << std::endl;
-    begin = std::chrono::high_resolution_clock::now();
-
-    DoFractalThreaded();
-
-    for (auto&& th : threads)
-    {
-        th.join();
-    }
-    end = std::chrono::high_resolution_clock::now();
-    std::cout << "Rendering finished!" << std::endl;
-    timeSec = timePast(begin, end);
-    timeMili = timePast<std::chrono::milliseconds>(begin, end);
-
-    std::stringstream ss;
-    ss << "Time taken to render Lyapunov Fractal: " << timeSec << " sec " << timeMili - (timeSec * 1000) << " milisec";
-    std::cout << "Time taken to render Lyapunov Fractal: " << timeSec << " sec " << timeMili - (timeSec * 1000) << " milisec" << std::endl;
-    std::cout << "Fractal string used to generate image: " << fractString << std::endl;
-    std::cout << ITERATIONS << " iterations per pixel" << std:: endl;
-
-    SDL_SetWindowTitle(m_Window, ss.str().c_str());
-    SwapBuffers();
-    waitForUserExit();
-
-    SDL_DestroyWindow(m_Window);
-    SDL_Quit();
-    return 0;
-}
-*/
+    // We must indicate the image’s color type and what format to save as
+    image::ImageLuma8(imgbuf).save(fout, image::PNG);
 }
